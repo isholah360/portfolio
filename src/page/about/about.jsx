df1edb4f8f9158c5482304f16f7af76063816dac
ishola360 <isholah360@gmail.com>
2022-12-22 06:48:57 -0800

    update 4

--- src/page/about/about.jsx
+++ src/page/about/about.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
+import './about.css'
+import gsap from 'gsap';
+import { Power3 } from 'gsap/src/index';
+import { TypeAnimation } from 'react-type-animation';
+
+
 
 function About() {
+
+  const node1 = useRef(null)
+  const node2 = useRef(null)
+  const node3 = useRef(null)
+  const node11 = useRef(null)
+  const node22 = useRef(null)
+  const node33 = useRef(null)
+  const writer = useRef(null)
+
+  useEffect(()=>{
+    gsap.fromTo([writer.current], {opacity:1, color:"white",}, { color:"black", opacity:0,
+      duration:3, ease:Power3.outIn})
+    gsap.fromTo([node11.current, node22.current, node33.current], { width:"30rem", x:"0", opacity:1}, { opacity:0, width:"0rem", x:"30rem",
+      duration:1.5, ease:Power3.outIn, transformOrigin:'left', delay:6,
+      stagger:{amount:1.5}})
+
+    gsap.fromTo([node1.current, node2.current, node3.current], { opacity:1, height:"20rem", y:0}, { opacity:0, height:"0rem", y:"20rem",
+      duration:2.5, ease:Power3.outIn, transformOrigin:'left', delay:2,
+      stagger:{amount:1.5}})
+  },[])
+
+
   return (
     <>
       <div>
-        About us
+         <div className="bigger">
+        <div ref={writer} className="writer">Welcome</div>
+            <div className="nodes">
+                  <div ref={node1} className="node1"></div>
+                  <div ref={node2} className="node2"></div>
+                  <div ref={node3} className="node3"></div>
+            </div>
+            <div className="nodez">
+                  <div className="nods">
+                    <div ref={node11} className="node11"></div>
+                    <div ref={node22} className="node22"></div>
+                    <div ref={node33} className="node33"></div>
+                  </div>
+            </div>
+         </div>
       </div>
     </>
   );
--- src/page/homepage/homepage.jsx
+++ src/page/homepage/homepage.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
+import './homepage.css';
+import Typewriter from 'typewriter-effect';
+import { TypeAnimation } from 'react-type-animation';
 
 function Homepage() {
+  
   return (
     <>
       <div>
-        <h1>Homepage</h1>
+        <div className="bg-bg">
+            <div className="bg-img">
+                  <div className="headercontent">
+                    <div className="name">
+                      Ishola Duro
+                    </div>
+                    <div className="thedev">
+                      <div>I'm a</div>
+                        <div>
+                            <TypeAnimation
+                                sequence={[
+                                  'Developer', 
+                                  2500, 
+                                  'Freelancer', 
+                                  2500, 
+                                  'Designer', 
+                                  2500,
+                                  () => {
+                                    console.log('Done typing!'); 
+                                  }
+                                ]}
+                                wrapper="div"
+                                speed= {5}
+                                deletionSpeed={7}
+                                cursor={true}
+                                repeat={Infinity}
+                                style={{ fontSize: '1em' }}
+                              />
+                        </div>
+                      </div>
+                  </div>
+            </div>
+                 <div className="social-media">
+                    <img className='media' src="/assets/instagram.png" alt="" />
+                    <img className='media' src="/assets/facebook.png" alt="" />
+                    <img className='media' src="/assets/twitter.png" alt="" />
+                    <img className='media' src="/assets/github.png" alt="" />
+                    <img className='media' src="/assets/linkedin-logo.png" alt="" />
+                  </div>
+        </div>
       </div>
     </>
   );
