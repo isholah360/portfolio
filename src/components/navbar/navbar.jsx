41c082141c33d5c5b7879c455ace01e2bef211ff
ishola360 <isholah360@gmail.com>
2022-12-22 15:30:39 -0800

    update 5

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './navbar.css'
+
+const Navbar = ()=>{
+    return(
+        <>
+        <div>
+            <div className="navbar">
+                <ul>
+                    <li>About</li>
+                    <li>Resume</li>
+                    <li>Portfolio</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
